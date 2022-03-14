69280ad2ca89c100357bd258cef9174da8bf7f72
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-14 21:12:19 +0530

    page header and some extras

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref,reactive } from 'vue';
+
+import Header from './components/Header.vue';
+
+const smth = ref("Some text");
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Header/>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 100vw;
+  margin: 40px;
 }
+
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="page-header">
+        <div :class="head_left_class">
+            <div class="head-logo">
+                <img src="./../assets/foodbowl.png" alt="foodbowllogo">
+            </div>
+            <h1><a href="">Diet-Bowl</a></h1>
+        </div>
+        <div :class="head_right_class">
+            <h2><a href="">Features</a></h2>
+            <h2><a href="">Download</a></h2>
+            <h2><a href="">About Us</a></h2>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const head_left_class = ref('head-left');
+    const head_right_class = ref('head-right');
+</script>
+
+<style>
+
+.page-header{
+    display: flex;
+}
+
+.head-logo{
+    width: 80px;
+    height: 80px;
+    padding: 5px;
+    border-radius: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #E8F4FA;
+    box-shadow: 4px 4px 8px 0px rgba(0,0,0,0.1),-4px -4px 8px 0px rgba(0,0,0,0.1);
+    -webkit-box-shadow: 4px 4px 8px 0px rgba(0,0,0,0.1),-4px -4px 8px 0px rgba(0,0,0,0.1);
+    -moz-box-shadow: 4px 4px 8px 0px rgba(0,0,0,0.1),-4px -4px 8px 0px rgba(0,0,0,0.1);
+}
+
+.head-left{
+    display: flex;
+    gap: 20px;
+    margin-right: auto;
+    justify-content: center;
+    align-items: center;
+}
+
+.head-right{
+    display: flex;
+    gap: 80px;
+    justify-content: center;
+    align-items: center;
+}
+
+h1{
+    font-family: 'Lato';
+    font-weight: 700;
+    color: #0d8b1e;
+}
+
+img{
+    max-height: 100px;
+    max-width: 100px;
+    height: auto;
+    width: auto;
+}
+
+a{
+    color: black;
+    text-decoration: none;
+}
+
+</style>
